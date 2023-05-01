2b2570cea07b7efc465e13b5c139f327fa775968
Brian <bpl.law@gmail.com>
2023-05-01 20:05:41 +1000

    fixed the build data script in regards to filepaths

--- 0_Build_Data.py
+++ 0_Build_Data.py
@@ -3,42 +3,45 @@
 # MAGIC This is an example for how to use Langchain
 
 # COMMAND ----------
+import os
 
 # We will setup a folder to store the files
 username = spark.sql("SELECT current_user()").first()['current_user()']
 username
 
+dbfs_path = f'/home/{username}/pdf_data'
+os.environ['DATASTASH_FOLDER'] = dbfs_path
 # COMMAND ----------
 
 # we will use this dbfs folder
-dbutils.fs.mkdirs(f'/home/{username}/pdf_data')
+dbutils.fs.mkdirs(dbfs_path)
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2304.09151.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2304.09151.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2212.10264.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2212.10264.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2109.07306.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2109.07306.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2105.00572.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2105.00572.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2210.14867.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2210.14867.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget https://arxiv.org/pdf/2010.11934.pdf -U me-me-me -P /dbfs/home/brian.law@databricks.com/pdf_data
+# MAGIC wget https://arxiv.org/pdf/2010.11934.pdf -U me-me-me -P /dbfs$DATASTASH_FOLDER
