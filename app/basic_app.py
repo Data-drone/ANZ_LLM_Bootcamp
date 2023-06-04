ad4407b4b7f924b6ffaf778f7ef3c432a11744b2
Brian <bpl.law@gmail.com>
2023-06-04 23:33:25 +1000

    switched over to gradio app

--- app/basic_app.py
+++ app/basic_app.py
@@ -1,5 +1,12 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import gradio as gr
 
-st.title('Fonterra Test App')
+fastapi_app = FastAPI()
+gradio_app = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
+
+@fastapi_app.get("/")
+def redirect_to_gradio():
+    return RedirectResponse(url="/gradio")
+
+app = gr.mount_gradio_app(fastapi_app, gradio_app, path='/gradio')
--- app/start_app.py
+++ app/start_app.py
@@ -1,20 +1,25 @@
 # Databricks notebook source
-# MAGIC %pip install uvicorn gradio==3.20.0
-
+# MAGIC %pip install fastapi==0.95.2 gradio==3.20.1 uvicorn
 
 # COMMAND ----------
+import os
+
+# COMMAND ----------
+app_port = 7777
+os.environ['DB_APP_PORT'] = f'{app_port}'
 
 cluster_id = spark.conf.get("spark.databricks.clusterUsageTags.clusterId")
+
 workspace_url = spark.conf.get("spark.databricks.workspaceUrl")
 org_id = spark.conf.get("spark.databricks.clusterUsageTags.clusterOwnerOrgId")
-endpoint_url = f"https://{workspace_url}/driver-proxy-api/o/{org_id}/{cluster_id}/7777/"
+
+# AWS this works at least
+proxy_prefix = f'dbc-dp-{org_id}.cloud.databricks.com'
+endpoint_url = f"https://{proxy_prefix}/driver-proxy/o/{org_id}/{cluster_id}/{app_port}/"
 print(f"Access this API at {endpoint_url}")
 
 # COMMAND ----------
 
-!streamlit run app.py
+!uvicorn basic_app:app --host 0.0.0.0 --port $DB_APP_PORT
 
 # COMMAND ----------
-
-# app:app refers to app function inside the app file
-# !uvicorn --host 0.0.0.0 --port 7777 app:app --root-path 
