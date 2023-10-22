24a284a67478acde6896ce57c7b616fef0a26105
Brian <bpl.law@gmail.com>
2023-10-22 23:17:10 +1100

    added some extra models for testing with endpoints

--- 0_lab_setup.py
+++ 0_lab_setup.py
@@ -134,7 +134,10 @@ huggingface_hub.login(token=huggingface_key)
 from huggingface_hub import hf_hub_download, list_repo_files
 
 repo_list = {'llama_2_gpu': 'meta-llama/Llama-2-7b-chat-hf',
-             'llama_2_cpu': 'TheBloke/Llama-2-7B-chat-GGUF'}
+             'llama_2_cpu': 'TheBloke/Llama-2-7B-chat-GGUF',
+             'llama_2_awq': 'TheBloke/Llama-2-7B-AWQ',
+             'llama_2_13b_awq': 'TheBloke/Llama-2-13B-chat-AWQ',
+             'vicuna_1.5_13b_awq': 'TheBloke/vicuna-13B-v1.5-16K-AWQ'}
 
 for lib_name in repo_list.keys():
     for name in list_repo_files(repo_list[lib_name]):
